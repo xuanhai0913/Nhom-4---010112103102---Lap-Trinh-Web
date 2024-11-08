ceedc8eec19ebecaf159ecff01b41341dbe9db1c
bedive <trungbed1ve@gmail.com>
2024-11-08 19:37:19 +0700

    all

--- src/PHP/pages/reset.php
+++ src/PHP/pages/reset.php
@@ -17,27 +17,25 @@
     <article class="container">
         <div class="wrapper">
             <div class="form-container" id="form">
-                <div class="section__form section__form--signUp">
-                    <form id="form-reset" class="form-reset">
-                        <h1>Reset Password</h1>
-                        <span>Đặt lại mật khẩu của bạn.</span>
-                        <div class="input-form">
-                            <input id="input__reset" class="input" type='text' placeholder="Xác nhận mật khẩu" name="password-first">
-                            <span id="message__password-first" class="message"></span>
-                        </div>
-                        <div class="input-form">
-                            <input id="input__reset" class="input" type='text' placeholder="Xác nhận mật khẩu" name="password-second">
-                            <span id="message__password-second" class="message"></span>
-                        </div>
-                        <button type="submit">Xác nhận</button>
-                    </form>
-                </div>
+                <form id="form-reset" class="form-reset">
+                    <h1>Reset Password</h1>
+                    <span>Đặt lại mật khẩu của bạn.</span>
+                    <div class="input-form">
+                        <input id="input__password-first" class="input" type='text' placeholder="Xác nhận mật khẩu" name="password-first">
+                        <span id="message__password-first" class="message" ></span>
+                    </div>
+                    <div class="input-form">
+                        <input id="input__password-second" class="input" type='text' placeholder="Xác nhận mật khẩu" name="password-second">
+                        <span id="message__password-second" class="message" ></span>
+                    </div>
+                    <button type="submit">Xác nhận</button>
+                </form>
             </div>
         </div>
-        <article class="container">
+    <article class="container">
 </body>
 
-<script src="../../assets/js/script.js"></script>
+<script src="../../assets/js/reset.js"></script>
 
 
 </html>
---
+++ src/assets/js/reset.js
@@ -0,0 +1,34 @@
+$('#form-reset').submit(function (e) {
+    e.preventDefault();
+    clearErrors();
+    $.ajax({
+        type: 'POST',
+        url: '../../PHP/auth/resetPassword.php',
+        data: $(this).serialize(),
+        dataType: 'json',
+        success: function (response) {
+            console.log(response);
+            if (response.status === 'error') {
+                showError(response.object, response.message, response.form);
+            } else {
+                location = '../../PHP/pages/index.php';
+            }
+        }
+    });
+});
+function showError(object, message, form) {
+    document.querySelector(`#input__${object}-${form}`).classList.add('input--error');
+    document.querySelector(`#message__${object}-${form}`).innerHTML = "<i class='fas fa-exclamation-circle'></i> " + message;
+    document.querySelector(`#message__${object}-${form}`).style.display = 'block';
+}
+
+function clearErrors() {
+    // Xóa lớp 'input--error' và ẩn tất cả thông báo lỗi
+    document.querySelectorAll('.input--error').forEach(element => {
+        element.classList.remove('input--error');
+    });
+    document.querySelectorAll('.message').forEach(element => {
+        element.style.display = 'none';
+        element.innerHTML = '';
+    });
+}
