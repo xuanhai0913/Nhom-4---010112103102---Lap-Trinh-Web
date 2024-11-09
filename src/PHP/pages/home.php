f613c24c35c394b4df48298ba68be1a2e1f8fdc8
Nguyễn Xuân Hải <73636134+xuanhai0913@users.noreply.github.com>
2024-11-09 22:17:44 +0700

    Update Room, CreateRoom, JoinRoom and Frontend
    Update Room, CreateRoom, JoinRoom and Frontend.
    Update room.css use header.php

--- src/PHP/pages/home.php
+++ src/PHP/pages/home.php
@@ -8,6 +8,9 @@
     <link href="../../assets/css/base.css" rel="stylesheet" />
     <link href="../../assets/css/home.css" rel="stylesheet" />
     <script src="../../assets/js/jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="../room/config.js"></script>
+
 </head>
 
 <body>
@@ -16,9 +19,13 @@
         <div class="home-container">
             <div class="content">
                 <h1>V2meet – Kết nối trái tim, xóa tan khoảng cách</h1>
-                <p>Kết nối, cộng tác và ăn mừng mọi khoảnh khắc – chỉ với một cú chạm, khoảng cách sẽ không còn là rào cản!</p>
+                <p>Kết nối, cộng tác và ăn mừng mọi khoảnh khắc – chỉ với một cú chạm, khoảng cách sẽ không còn là rào
+                    cản!</p>
 
                 <div class="function">
+                    <input type="text" id="roomNameInput" placeholder="Nhập tên phòng">
+                    <input type="text" id="uniqueNameInput" placeholder="Nhập tên riêng">
+
                     <button class="function__create-room" id="createRoomButton">
                         <i class="fa-solid fa-plus"></i>
                         Cuộc họp mới
@@ -37,13 +44,15 @@
             <div class="carousel">
                 <div class="carousel__thumbnails">
                     <i class="fas fa-chevron-left thumbnail__prev" onclick="prevSlide()"></i>
-                    <img alt="info" id="thumbnail__image" class="thumbnail__image" src="../../assets/images/static/home/info1.png" />
+                    <img alt="info" id="thumbnail__image" class="thumbnail__image"
+                        src="../../assets/images/static/home/info1.png" />
                     <i class="fas fa-chevron-right thumbnail__next" onclick="nextSlide()"></i>
                 </div>
                 <p id="carousel__text" class="carousel__text">
                     Nhận đường liên kết bạn có thể chia sẻ
                 </p>
-                <p class="carousel__description">Nhấp vào <strong>Cuộc họp mới</strong> để nhận đường liên kết mà bạn có thể gửi cho những người mình muốn họp cùng</p>
+                <p class="carousel__description">Nhấp vào <strong>Cuộc họp mới</strong> để nhận đường liên kết mà bạn có
+                    thể gửi cho những người mình muốn họp cùng</p>
                 <div class="carousel__dots">
                     <span class="carousel__dot carousel__dot--active" onclick="currentSlide(0)"></span>
                     <span class="carousel__dot" onclick="currentSlide(1)"></span>
